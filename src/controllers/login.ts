e981bab43f03441eae8dcde78350346b79f91c45
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-11-25 23:48:47 -0300

    add verification login credentials

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
+import prisma from "../prisma";
 
 class login {
-    async handle(request: Request, response: Response) {
-        const { email, pw } = request.body;
-   
-        response.json({ email, pw });
-     
-        console.log(email, pw)
+  async handle(request: Request, response: Response) {
+    const { email, pw } = request.body;
+
+    const user = await prisma.users.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user && user.pw === pw) {
+      response.status(200).json({ error: "Login successful" });
+      console.log("Login successful");
+    } else {
+      response.status(400).json({ error: "Invalid email or password" });
+      console.log("Invalid email or password");
     }
+  }
 }
 
 export { login };
