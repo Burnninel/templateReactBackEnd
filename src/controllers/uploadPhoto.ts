f3a4fb1ea51abde428b81716d7027f8a2de61aef
Burnninel <79659103+Burnninel@users.noreply.github.com>
2024-01-11 18:20:02 -0300

    verify token update photo

--- src/controllers/uploadPhoto.ts
+++ src/controllers/uploadPhoto.ts
@@ -1,23 +1,34 @@
 import { Request, Response } from "express"
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 class uploadPhoto {
   async handle(request: Request, response: Response) {
     try {
-      const file = request.file
-      const validImg = file.mimetype.match(/^image\//)
+      const token = request.headers.authorization;
 
-      if(!file) {
-         console.log('Nenhum arquivo selecionado!')
-         return response.status(400).json({error: 'Nenhum arquivo selecionado!'})
+      const userVerify = jwt.verify(
+        token,
+        process.env.SECRET_ACCESS_TOKEN
+      ) as JwtPayload;
+
+      if(userVerify) {
+          const file = request.file
+          const validImg = file.mimetype.match(/^image\//)
+    
+          if(!file) {
+             console.log('Nenhum arquivo selecionado!')
+             return response.status(400).json({error: 'Nenhum arquivo selecionado!'})
+          }
+    
+          if (!validImg) {
+            console.log('Formato invalido!')
+            return response.status(400).json({error: 'Formato invalido!'})
+          } 
+    
+          console.log('Arquivo carregado com sucesso!')
+          return response.status(200).json({ message: 'Arquivo carregado com sucesso!', file: request.file })
       }
 
-      if (!validImg) {
-        console.log('Formato invalido!')
-        return response.status(400).json({error: 'Formato invalido!'})
-      } 
-
-      console.log('Arquivo carregado com sucesso!')
-      return response.status(200).json({ message: 'Arquivo carregado com sucesso!', file: request.file })
     } catch (error) {
       console.log('deu ruim')
       response.status(400).json({ error: error.message })
