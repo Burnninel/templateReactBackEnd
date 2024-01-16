bfaa7ada773455e0788109b6bf00dc5e2af6c986
Burnninel <79659103+Burnninel@users.noreply.github.com>
2024-01-15 23:05:43 -0300

    Implementing validations on account creation + code refactoring

--- src/controllers/createAccount.ts
+++ src/controllers/createAccount.ts
@@ -1,10 +1,25 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 import prisma from '../prisma'
 
 class createAccount {
     async handle(request: Request, response: Response) {
-        const { name, profession, email, pw } = request.body;
-        
+        const { name, profession, email, pw } = request.body
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+        const user = await prisma.users.findUnique({
+            where: {
+              email: email,
+            },
+        }) 
+
+        if(user) {
+            return response.status(400).json({ error: "Email already registered!" })
+        }
+
+        if(!emailRegex.test(email)) {
+            return response.status(400).json({ error: "Invalid email!" })
+        }
+
         const dataAccount = await prisma.users.create({
             data: {
                 name,
@@ -16,8 +31,8 @@ class createAccount {
             }
         })
 
-        return response.json(dataAccount)
+        return response.status(200).json(dataAccount)
     }
 }
 
-export { createAccount };
+export { createAccount }
