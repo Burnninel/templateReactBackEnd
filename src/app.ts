0442db26e116cda611696dc3b8b4cbaada090dee
Burnninel <79659103+Burnninel@users.noreply.github.com>
2024-01-17 22:55:34 -0300

    refactoring code

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import cookieParser from 'cookie-parser';
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import cookieParser from 'cookie-parser'
 import session from 'express-session'
-import { router } from "./routes";
-import { SECRET_ACCESS_TOKEN } from './config';
+import { router } from "./routes"
+import { SECRET_ACCESS_TOKEN } from './config'
 
-const app = express();
+const app = express()
 
 declare global {
   namespace Express {
@@ -16,25 +16,25 @@ declare global {
   }
  }
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(cookieParser());
+app.use(cookieParser())
 
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
-}));
+}))
 
 app.use(session({
   secret: SECRET_ACCESS_TOKEN,
   resave: false,
   saveUninitialized: true,
-}));
+}))
 
 app.use(router);
 
 app.use('/uploads', express.static('uploads'));
 
 app.listen(4000, () => {
-  console.log("Server is running on port 4000");
-});
+  console.log("Server is running on port 4000")
+})
